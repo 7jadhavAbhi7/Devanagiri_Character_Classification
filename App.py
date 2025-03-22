ef1fbc548612f62c3924fbaa5488816c81fc7228
Abhi <abhijadhav31122002@gmail.com>
2025-03-22 23:01:40 +0530

    Huh Again Commit

--- App.py
+++ App.py
@@ -58,4 +58,4 @@ def predict():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Vercel uses dynamic ports
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000, debug=True)  # Run the ap
