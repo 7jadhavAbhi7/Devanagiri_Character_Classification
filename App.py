2f85865d13fe2bfbad6e69c29d26000992cfefde
Abhi <abhijadhav31122002@gmail.com>
2025-03-22 22:22:29 +0530

    Fifth Commit

--- App.py
+++ App.py
@@ -57,4 +57,5 @@ def predict():
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Vercel uses dynamic ports
+    app.run(host="0.0.0.0", port=port)
